26b375b47c30ba287dc7536d5e99725d7021e95b
Jacoby Johnson <jacobyajohnson@gmail.com>
2023-01-01 16:35:49 -0600

    Commented out unfunctional menu items temporarily for production build

--- src/ui/pages/EditorPage.tsx
+++ src/ui/pages/EditorPage.tsx
@@ -14,24 +14,24 @@ function EditorPage() {
   return (
     <div className={editorPageStyles["editor-page"]}>
       <nav className={editorPageStyles["menu-bar"]}>
-        <Menu className={editorPageStyles["menu"]} transition menuClassName={editorPageStyles["menu-drop-down"]} menuButton={<MenuButton className={editorPageStyles["menu-button"]}> File </MenuButton>}>
+        {/* <Menu className={editorPageStyles["menu"]} transition menuClassName={editorPageStyles["menu-drop-down"]} menuButton={<MenuButton className={editorPageStyles["menu-button"]}> File </MenuButton>}>
             <MenuItem  className={editorPageStyles["menu-item"]}>New</MenuItem>
             <MenuItem className={editorPageStyles["menu-item"]}>< input type="file" accept="image/*"/> Open</MenuItem>
             <MenuItem className={editorPageStyles["menu-item"]}>Save</MenuItem>
             <MenuItem className={editorPageStyles["menu-item"]}>Save As</MenuItem>
             <MenuItem className={editorPageStyles["menu-item"]}>Export</MenuItem>
             <MenuItem className={editorPageStyles["menu-item"]}>Import</MenuItem>
-        </Menu>
+        </Menu> */}
 
-        <Menu className={editorPageStyles["menu"]} transition menuClassName={editorPageStyles["menu-drop-down"]} menuButton={<MenuButton  className={editorPageStyles["menu-button"]}> Edit </MenuButton>}>
+        {/* <Menu className={editorPageStyles["menu"]} transition menuClassName={editorPageStyles["menu-drop-down"]} menuButton={<MenuButton  className={editorPageStyles["menu-button"]}> Edit </MenuButton>}>
           <MenuItem className={editorPageStyles["menu-item"]}>Undo</MenuItem>
           <MenuItem className={editorPageStyles["menu-item"]}>Redo</MenuItem>
-        </Menu>
+        </Menu> */}
 
-        <Menu className={editorPageStyles["menu"]} transition menuClassName={editorPageStyles["menu-drop-down"]} menuButton={<MenuButton  className={editorPageStyles["menu-button"]}> View </MenuButton>}>
+        {/* <Menu className={editorPageStyles["menu"]} transition menuClassName={editorPageStyles["menu-drop-down"]} menuButton={<MenuButton  className={editorPageStyles["menu-button"]}> View </MenuButton>}>
           <MenuItem className={editorPageStyles["menu-item"]} > Theme </MenuItem>
           <MenuItem className={editorPageStyles["menu-item"]}> Toggle Fullscreen </MenuItem>
-        </Menu>
+        </Menu> */}
 
         <Menu className={editorPageStyles["menu"]} transition menuClassName={editorPageStyles["menu-drop-down"]} menuButton={<MenuButton  className={editorPageStyles["menu-button"]}> Select Editor </MenuButton>}>
           <MenuItem className={editorPageStyles["menu-item"]} onClick={() => setCurrentBoardType("LIFELIKE")}>Bounded Life Like Editor</MenuItem>
