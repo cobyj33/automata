3cd4b4b93fc71d5a74c6ba238e4e734730234fcf
Jacoby Johnson <jacobyajohnson@gmail.com>
2023-01-14 17:33:25 -0600

    Changed LayeredCanvas to be a little more readable

--- src/ui/components/LayeredCanvas.tsx
+++ src/ui/components/LayeredCanvas.tsx
@@ -1,4 +1,5 @@
 import layeredCanvasStyles from "ui/components/styles/LayeredCanvas.module.css"
-export const LayeredCanvas = ({ children, className = "" }: { children: any, className?: string }) => <div className={`${layeredCanvasStyles["layered-canvas"]} ${className}`}> { children } </div>
-
+export function LayeredCanvas({ children, className = "" }: { children: any, className?: string }) { 
+    return <div className={`${layeredCanvasStyles["layered-canvas"]} ${className}`}> { children } </div>
+}
 export default LayeredCanvas
