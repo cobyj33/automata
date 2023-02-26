e84b4efcbe445c094e318b4603540ef6e1fc32ea
Jacoby Johnson <jacobyajohnson@gmail.com>
2023-02-26 09:03:56 -0600

    chore: removed margin from ActionButton, as it caused layout problems in the editor mode toolbar

--- src/ui/components/reuse/ActionButton.tsx
+++ src/ui/components/reuse/ActionButton.tsx
@@ -4,7 +4,7 @@ interface ActionButtonProps extends Omit<React.ButtonHTMLAttributes<HTMLButtonEl
     error?: boolean
 }
 
-const baseStyles = "p-1 text-white bg-zinc-800 rounded-lg m-2"
+const baseStyles = "p-1 text-white bg-zinc-800 rounded-lg"
 const styles = baseStyles.concat(" hover:border-b hover:border-b-white hover:bg-zinc-600 active:border-2 active:border-white")
 const errorStyles = baseStyles.concat(" hover:border-b hover:border-b-red hover:bg-zinc-600 active:border-b-red-500 active:border-b-2 active:text-red-500")
 
