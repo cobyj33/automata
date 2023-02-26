771faf62906ae9f22ac019d9344d8797ad5b9d47
Jacoby Johnson <jacobyajohnson@gmail.com>
2023-02-25 21:29:10 -0600

    Chaned src/Tests to src/tests

--- src/Tests/data.test.ts
+++
@@ -1,7 +0,0 @@
-import { NAMED_LIFE_RULES_TO_RULE_STRINGS } from "data";
-import { isValidLifeString } from "common/generationFunctions";
-
-test("Life Rules Data Test - All Life Strings Valid", () => {
-    const ruleStrings = Object.values(NAMED_LIFE_RULES_TO_RULE_STRINGS)
-    expect(ruleStrings.map(ruleString => isValidLifeString(ruleString)).every(val => val === true)).toBe(true)
-})
