a2c757f3f2c6e32ebe9df05a83f228c8b92f9c49
Jacoby Johnson <jacobyajohnson@gmail.com>
2023-02-26 07:57:35 -0600

    chore: Changed SideBarEditorToolStyles to SideBarEditorToolProps (misnamed variable)

--- src/ui/components/reuse/editor/SideBarEditorTool.tsx
+++ src/ui/components/reuse/editor/SideBarEditorTool.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const styles = "bg-zinc-900 rounded-lg text-center px-3 py-2 m-2 hover:border-b-2 hover:border-b-white flex flex-col"
 
-interface SideBarEditorToolStyles extends Omit<React.HTMLAttributes<HTMLDivElement>, "className"> {}
 
-export const SideBarEditorTool = (props: SideBarEditorToolStyles) => {
+interface SideBarEditorToolProps extends Omit<React.HTMLAttributes<HTMLDivElement>, "className"> {}
+
+export const SideBarEditorTool = (props: SideBarEditorToolProps) => {
   return (
     <div className={styles} {...props} />
   )
