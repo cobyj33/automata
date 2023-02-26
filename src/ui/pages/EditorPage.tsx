fd329526138b5e7b2b10192839304f36cf365503
Jacoby Johnson <jacobyajohnson@gmail.com>
2023-02-25 21:57:12 -0600

    Removed react-menu menubar in favor of a more basic hover-drop-down style navbar Removed imports to react-menu Removed styles for react-menu menubar
    react-menu may and probably will return in the future though as an editor-specific menubar

--- src/ui/pages/EditorPage.tsx
+++ src/ui/pages/EditorPage.tsx
@@ -2,62 +2,39 @@ import { useState } from 'react';
 import { IVector2 } from 'common/Vector2';
 import editorPageStyles from 'ui/pages/styles/EditorPage.module.css';
 
-import { Menu, MenuItem, MenuButton, SubMenu } from "@szhsin/react-menu"
-import { CellMatrix } from 'common/CellMatrix';
 import LifeLikeEditor from 'ui/components/LifeLikeEditor';
 import ElementaryBoard from 'ui/components/ElementaryBoard';
-import { Box } from 'common/Box';
+import ToggleButton from 'ui/components/reuse/ToggleButton';
 
 export type BoardType = "LIFELIKE" | "ELEMENTARY"
 const DEFAULT_ELEMENTARY_EDITOR_DATA_WIDTH = 1000;
 
-function EditorPage() {
-  const [lifeLikeBoard, setLifeLikeBoard] = useState<IVector2[]>([])
-  const [elementaryBoard, setElementaryBoard] = useState<number[]>(new Array<number>(DEFAULT_ELEMENTARY_EDITOR_DATA_WIDTH).fill(0))
+function EditorPage(): JSX.Element {
+    const [currentBoard, setCurrentBoard] = useState<BoardType>("LIFELIKE");
+    const [lifeLikeBoard, setLifeLikeBoard] = useState<IVector2[]>([])
+    const [elementaryBoard, setElementaryBoard] = useState<number[]>(new Array<number>(DEFAULT_ELEMENTARY_EDITOR_DATA_WIDTH).fill(0))
 
-  function typeToBoard(boardType: BoardType): JSX.Element {
-    switch (boardType) {
-      case "LIFELIKE": return <LifeLikeEditor boardData={[lifeLikeBoard, setLifeLikeBoard]} />
-      case "ELEMENTARY": return <ElementaryBoard boardData={[elementaryBoard, setElementaryBoard]} />
+    function typeToBoard(boardType: BoardType): JSX.Element {
+        switch (boardType) {
+            case "LIFELIKE": return <LifeLikeEditor boardData={[lifeLikeBoard, setLifeLikeBoard]} />
+            case "ELEMENTARY": return <ElementaryBoard boardData={[elementaryBoard, setElementaryBoard]} />
+        }
     }
-  }
 
-  const [currentBoard, setCurrentBoard] = useState<BoardType>("LIFELIKE");
 
-  return (
+    return (
     <div className={editorPageStyles["editor-page"]}>
-      <nav className={editorPageStyles["menu-bar"]}>
-        {/* <Menu className={editorPageStyles["menu"]} transition menuClassName={editorPageStyles["menu-drop-down"]} menuButton={<MenuButton className={editorPageStyles["menu-button"]}> File </MenuButton>}>
-            <MenuItem  className={editorPageStyles["menu-item"]}>New</MenuItem>
-            <MenuItem className={editorPageStyles["menu-item"]}>< input type="file" accept="image/*"/> Open</MenuItem>
-            <MenuItem className={editorPageStyles["menu-item"]}>Save</MenuItem>
-            <MenuItem className={editorPageStyles["menu-item"]}>Save As</MenuItem>
-            <MenuItem className={editorPageStyles["menu-item"]}>Export</MenuItem>
-            <MenuItem className={editorPageStyles["menu-item"]}>Import</MenuItem>
-        </Menu> */}
+        <nav className={editorPageStyles["menu-bar"]}>
+            <ToggleButton selected={currentBoard === "LIFELIKE"} onClick={() => setCurrentBoard("LIFELIKE")}> Life-Like Editor </ToggleButton>
+            <ToggleButton selected={currentBoard === "ELEMENTARY"} onClick={() => setCurrentBoard("ELEMENTARY")}> Elementary Editor </ToggleButton>
+        </nav>
 
-        {/* <Menu className={editorPageStyles["menu"]} transition menuClassName={editorPageStyles["menu-drop-down"]} menuButton={<MenuButton  className={editorPageStyles["menu-button"]}> Edit </MenuButton>}>
-          <MenuItem className={editorPageStyles["menu-item"]}>Undo</MenuItem>
-          <MenuItem className={editorPageStyles["menu-item"]}>Redo</MenuItem>
-        </Menu> */}
+        <div className={editorPageStyles["editor-area"]}>
+            { typeToBoard(currentBoard) }
+        </div>
 
-        {/* <Menu className={editorPageStyles["menu"]} transition menuClassName={editorPageStyles["menu-drop-down"]} menuButton={<MenuButton  className={editorPageStyles["menu-button"]}> View </MenuButton>}>
-          <MenuItem className={editorPageStyles["menu-item"]} > Theme </MenuItem>
-          <MenuItem className={editorPageStyles["menu-item"]}> Toggle Fullscreen </MenuItem>
-        </Menu> */}
-
-        <Menu className={editorPageStyles["menu"]} transition menuClassName={editorPageStyles["menu-drop-down"]} menuButton={<MenuButton  className={editorPageStyles["menu-button"]}> Select Editor </MenuButton>}>
-          <MenuItem className={editorPageStyles["menu-item"]} onClick={() => setCurrentBoard("LIFELIKE")}>Bounded Life Like Editor</MenuItem>
-          <MenuItem className={editorPageStyles["menu-item"]} onClick={() => setCurrentBoard("ELEMENTARY")}>Elementary Editor</MenuItem>
-        </Menu>
-      </nav>
-
-      <div className={editorPageStyles["editor-area"]}>
-        { typeToBoard(currentBoard) }
-      </div>
- 
     </div> 
-  );
+    );
 }
 
 
